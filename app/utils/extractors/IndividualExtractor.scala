cfbc69d4c00b74ab31d141a5104b2990384365ea
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-06-11 22:36:39 +0100

    Second case for individual or business page object cleanup tests.

--- app/utils/extractors/IndividualExtractor.scala
+++ app/utils/extractors/IndividualExtractor.scala
@@ -45,6 +45,8 @@ class IndividualExtractor {
           .flatMap(_.set(PassportOrIdCardPage, PassportOrIdCard.IdCard))
           .flatMap(_.set(IdCardPage, IdCard(countryOfIssue, number, expirationDate)))
       case CombinedPassportOrIdCard(countryOfIssue, number, expirationDate) =>
+        // TODO: field with path 'passport' is read as a CombinedPassportOrIdCard
+        // Need to think about how we can retrieve this information and know if it is a passport or ID card
         userAnswers.set(NinoYesNoPage, false)
     }
   }
