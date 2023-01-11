13f31bae706c0c17f9c5b3bb0f6963cd08586806
zmdnoor <zakwan.noor@digital.hmrc.gov.uk>
2023-01-11 14:26:25 +0000

    DDCE-3650 Fix for DOB field

--- app/controllers/individual/DateOfBirthController.scala
+++ app/controllers/individual/DateOfBirthController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ class DateOfBirthController @Inject()(
                                        @Individual navigator: Navigator
                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form: Form[LocalDate] = formProvider.withPrefix("individual.dateOfBirth")
+  def form: Form[LocalDate] = formProvider.withPrefix("individual.dateOfBirth")
 
   def onPageLoad(mode: Mode): Action[AnyContent] = actions.authWithIndividualName {
     implicit request =>
