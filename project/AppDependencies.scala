9744001498ac4288a9e04612ce644625c204f5d0
MRBeresford <125281117+MRBeresford@users.noreply.github.com>
2023-09-06 14:01:09 +0100

    DDCE-4195: Update to CSP (#104)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "7.20.0"
+  val boostrapVersion = "7.21.0"
   val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
-    play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.15.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % boostrapVersion,
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28"
@@ -20,8 +19,8 @@ object AppDependencies {
     "org.scalatest"               %% "scalatest"                % "3.2.16",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.0",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
