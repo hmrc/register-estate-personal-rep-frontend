5a7b7a2f6ba0344587c9ba8d118a12f76670f730
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2025-08-04 12:07:04 +0100

    DCE-6956 - For register-estate-personal-rep-frontend, removed org.wiremock:wiremock-standalone:test dependency, as it already comes from bootstrap-test.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,6 @@ object AppDependencies {
     "org.scalatest"               %% "scalatest"                % "3.2.19",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
     "org.jsoup"                   %  "jsoup"                    % "1.21.1",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.13.0",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
