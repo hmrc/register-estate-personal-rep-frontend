cc595759171ff4a26347debef3d6debd971c846d
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2025-08-04 11:43:48 +0100

    DCE-6956 - For register-estate-personal-rep-frontend, below dependencies versions updated: boostrapVersion = "9.19.0" mongoVersion = "2.7.0" sbtPlugin = "3.0.8" jsoup = "1.21.1"

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.11.0"
-  val mongoVersion = "2.6.0"
+  val bootstrapVersion = "9.19.0"
+  val mongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                     % mongoVersion,
@@ -18,7 +18,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion,
     "org.scalatest"               %% "scalatest"                % "3.2.19",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
-    "org.jsoup"                   %  "jsoup"                    % "1.20.1",
+    "org.jsoup"                   %  "jsoup"                    % "1.21.1",
     "org.wiremock"                %  "wiremock-standalone"      % "3.13.0",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
